bade83051549d5cea930c7f923ef415e34d5f9ac
BreMea <63671187+BreMea@users.noreply.github.com>
2023-01-04 13:59:01 -0500

    disable ads temp

--- src/lib/components/home/ad.svelte
+++ src/lib/components/home/ad.svelte
@@ -1,21 +1,3 @@
 <div class="h-32 mb-6 mt-6 lg:mb-0 w-0 min-w-full mbox flex flex-col justify-center items-center">
-	<div class="relative w-full max-w-full">
-		<script
-			async
-			src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5296928207981957"
-			crossorigin="anonymous"
-		></script>
-		<!-- hznt -->
-		<ins
-			class="adsbygoogle w-full"
-			style="display:block"
-			data-ad-client="ca-pub-5296928207981957"
-			data-ad-slot="3595922703"
-			data-ad-format="auto"
-			data-full-width-responsive="true"
-		/>
-		<script>
-			(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
-	</div>
+
 </div>
